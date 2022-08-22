20191e9bbb0327f2e3233b1b040435c493fcfa10
Adnan <eapple@EAPPLEs-MacBook-Pro.local>
2022-08-22 13:37:43 +0400

    array filter for searching

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+echo "<pre>";
+ $var = array(
+[
+'interval' => '2014-10-26',
+'leads'=> '0',
+'name' => 'CarEnquiry',
+'status' => 'NEW',
+'appointment' => '0'],
+[
+'interval' => '2022-10-26',
+'leads'=> '0',
+'name' => 'CarEnquiry1',
+'status' => 'NEW',
+'appointment' => 1],
+
+);
+
+
+
+
+$new = array_filter($var, function ($var) {
+    return ($var['name'] == 'CarEnquiry1');
+});
+
+print_r($new);
